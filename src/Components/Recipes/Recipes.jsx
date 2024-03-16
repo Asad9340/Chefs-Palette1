bee8f1f5f28bbdee34900390e73517d1baceeb08
Asad9340 <asadulimran1999@gmail.com>
2024-03-17 03:47:35 +0600

    added recipes section

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -1,13 +1,13 @@
-// import Recipe from "../Recipe/Recipe";
+import Recipe from "../Recipe/Recipe";
 
-// function Recipes({ recipes, handleCook }) {
-//   return (
-//     <div className="flex flex-col md:grid gap-4 md:grid-cols-2 m-3 md:m-6">
-//       {recipes.map((recipe, index) => (
-//         <Recipe key={index} recipe={recipe} handleCook={handleCook}></Recipe>
-//       ))}
-//     </div>
-//   );
-// }
+function Recipes({ recipes, handleCook }) {
+  return (
+    <div className="flex flex-col md:grid gap-4 md:grid-cols-2 m-3 md:m-6">
+      {recipes.map((recipe, index) => (
+        <Recipe key={index} recipe={recipe} handleCook={handleCook}></Recipe>
+      ))}
+    </div>
+  );
+}
 
-// export default Recipes
+export default Recipes
