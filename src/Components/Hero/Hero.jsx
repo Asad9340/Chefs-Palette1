77ac1eaf4db4a63023d91dcde0e8d7b2493416e8
Asad9340 <asadulimran1999@gmail.com>
2024-03-17 03:45:30 +0600

    added nvbar section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,27 +1,27 @@
-import './Hero.css';
-function Hero() {
-  return (
-    <div className="mx-auto flex max-w-6xl items-center justify-between">
-      <div className="banner rounded-3xl px-2 md:px-5 py-8 lg:px-52 lg:py-32 space-y-2 md:space-y-6 text-center m-3 md:m-6">
-        <h3 className=" text-white text-xl lg:text-4xl font-bold">
-          Discover an exceptional cooking <br /> class tailored for you!
-        </h3>
-        <p className="text-white text-xs lg:text-lg">
-          Step into our kitchen and lets cook up some magic together! From
-          savory classics to delectable desserts, weve got recipes to satisfy
-          every craving!
-        </p>
-        <div className="space-x-6">
-          <button className="rounded-md bg-[#0BE58A] text-white px-3 md:px-6 py-1 md:py-2  duration-300 font-semibold text-sm md:text-base">
-            Explore Now
-          </button>
-          <button className="rounded-md border border-white text-white px-3 md:px-6 py-1 md:py-2 hover:bg-[#0BE58A] duration-300 font-semibold  text-sm md:text-base">
-            Our Feedback
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+// import './Hero.css';
+// function Hero() {
+//   return (
+//     <div className="mx-auto flex max-w-6xl items-center justify-between">
+//       <div className="banner rounded-3xl px-2 md:px-5 py-8 lg:px-52 lg:py-32 space-y-2 md:space-y-6 text-center m-3 md:m-6">
+//         <h3 className=" text-white text-xl lg:text-4xl font-bold">
+//           Discover an exceptional cooking <br /> class tailored for you!
+//         </h3>
+//         <p className="text-white text-xs lg:text-lg">
+//           Step into our kitchen and lets cook up some magic together! From
+//           savory classics to delectable desserts, weve got recipes to satisfy
+//           every craving!
+//         </p>
+//         <div className="space-x-6">
+//           <button className="rounded-md bg-[#0BE58A] text-white px-3 md:px-6 py-1 md:py-2  duration-300 font-semibold text-sm md:text-base">
+//             Explore Now
+//           </button>
+//           <button className="rounded-md border border-white text-white px-3 md:px-6 py-1 md:py-2 hover:bg-[#0BE58A] duration-300 font-semibold  text-sm md:text-base">
+//             Our Feedback
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Hero;
+// export default Hero;
--- src/Components/OurRecipes/OurRecipes.jsx
+++ src/Components/OurRecipes/OurRecipes.jsx
@@ -1,16 +1,16 @@
-function OurRecipes() {
-  return (
-    <div className="max-w-6xl mx-auto px-3 md:px-6 space-y-4 md:space-y-6">
-      <h2 className="text-3xl md:text-4xl font-semibold text-center">
-        Our Recipes
-      </h2>
-      <p className="text-base text-gray-700 px-6 md:px-12 text-center">
-        Dive into a world of culinary delights with our varied recipes, spanning
-        from comforting classics to innovative creations, elevate your cooking
-        experience.
-      </p>
-    </div>
-  );
-}
+// function OurRecipes() {
+//   return (
+//     <div className="max-w-6xl mx-auto px-3 md:px-6 space-y-4 md:space-y-6">
+//       <h2 className="text-3xl md:text-4xl font-semibold text-center">
+//         Our Recipes
+//       </h2>
+//       <p className="text-base text-gray-700 px-6 md:px-12 text-center">
+//         Dive into a world of culinary delights with our varied recipes, spanning
+//         from comforting classics to innovative creations, elevate your cooking
+//         experience.
+//       </p>
+//     </div>
+//   );
+// }
 
-export default OurRecipes;
+// export default OurRecipes;
